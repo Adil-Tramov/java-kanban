52a7193219aca3e97903d171e20d625d3d84c969
Adil Tramov <adiltram420@gmail.com>
2025-10-16 20:46:20 +0300

    dd

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,30 +2,44 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
 
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
     List<Subtask> getEpicSubtasks(int epicId);
+
     List<Task> getHistory();
+
     List<Task> getPrioritizedTasks();
 }
