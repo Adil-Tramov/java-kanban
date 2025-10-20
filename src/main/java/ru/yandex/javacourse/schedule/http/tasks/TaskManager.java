507de1a74791676e79a8d42f9831f98c3f6f9938
Adil Tramov <adiltram420@gmail.com>
2025-10-20 03:01:52 +0300

    исправил структуру в TaskManager

--- src/main/java/ru/yandex/javacourse/schedule/http/tasks/TaskManager.java
+++ src/main/java/ru/yandex/javacourse/schedule/http/tasks/TaskManager.java
@@ -6,28 +6,39 @@ import java.util.Set;
 
 public interface TaskManager {
     List<Task> getTasks();
+
     List<Subtask> getSubtasks();
+
     List<Epic> getEpics();
 
     Task getTaskById(int id);
+
     Subtask getSubtaskById(int id);
+
     Epic getEpicById(int id);
 
     void createTask(Task task);
+
     void createSubtask(Subtask subtask);
+
     void createEpic(Epic epic);
 
     void updateTask(Task task);
+
     void updateSubtask(Subtask subtask);
+
     void updateEpic(Epic epic);
 
     void deleteTask(int id);
+
     void deleteSubtask(int id);
+
     void deleteEpic(int id);
 
     List<Subtask> getEpicSubtasks(int epicId);
 
     List<Task> getHistory();
+
     List<Task> getPrioritizedTasks();
 
     Set<Task> getIntersectionTasks(LocalDateTime start, LocalDateTime end);
