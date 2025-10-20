41a91228a8d24d0c65584ba7a72f5ca28f06e5b9
Adil Tramov <adiltram420@gmail.com>
2025-10-20 19:23:28 +0300

    исправил HttpTaskServer для Duration

--- src/ru/yandex/javacourse/schedule/http/HttpTaskServer.java
+++ src/ru/yandex/javacourse/schedule/http/HttpTaskServer.java
@@ -2,6 +2,12 @@ package ru.yandex.javacourse.schedule.http;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonDeserializationContext;
 import com.sun.net.httpserver.HttpServer;
 import ru.yandex.javacourse.schedule.http.handler.EpicHandler;
 import ru.yandex.javacourse.schedule.http.handler.HistoryHandler;
@@ -12,7 +18,9 @@ import ru.yandex.javacourse.schedule.http.managers.Managers;
 import ru.yandex.javacourse.schedule.http.tasks.TaskManager;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -52,11 +60,33 @@ public class HttpTaskServer {
 
     public static Gson getGson() {
         return new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, (com.google.gson.JsonDeserializer<LocalDateTime>) (json, type, context) ->
-                        LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .registerTypeAdapter(LocalDateTime.class, (com.google.gson.JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) ->
-                        context.serialize(src.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(Duration.class, new DurationAdapter())
                 .create();
+    }
+
+    static class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+        @Override
+        public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        }
+
+        @Override
+        public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+            return LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+    }
+
+    static class DurationAdapter implements JsonSerializer<Duration>, JsonDeserializer<Duration> {
+        @Override
+        public JsonElement serialize(Duration src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.toMinutes());
+        }
+
+        @Override
+        public Duration deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+            return Duration.ofMinutes(json.getAsLong());
+        }
     }
 
     public static void main(String[] args) {
